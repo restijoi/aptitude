718105009b4bd4c8a63d03c06e438a0f9136038c
Xavier Cagmat <xavier@tactivstudios.com>
2020-03-17 21:14:07 +0800

    created template

---
+++ frontend/assets/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {
+      textColor: {
+        'primary': '#5d14d3',
+        'secondary': '#b2b2b2'
+      },
+      spacing: {
+        '1/2': '.16rem',
+        14: '3.5rem',
+        19: '19rem'
+      },
+      fontSize: {
+        'xxs': '.5rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
