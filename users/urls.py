4b39d5f0f2393a198e15a5be222af35164d5da4f
John Paul Sargento <johnpaul@tactivstudios.com>
2020-03-19 13:51:54 +0800

    added test case

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path, include
 from users.views import UserRegistrationView, UserLoginView
 
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register('register', UserRegistrationView, basename='register')
-
 app_name = 'users'
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('register/', UserRegistrationView.as_view(), name='register'),
     path('auth/login/', UserLoginView.as_view(), name="login"),
 ]
